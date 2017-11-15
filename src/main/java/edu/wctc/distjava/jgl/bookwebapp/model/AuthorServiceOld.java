11cb03fb8f0d231d44a67ddd6f98fd2f81ba16b6
Nolan Marks <NMarks3@my.wctc.edu>
2017-11-14 19:47:24 -0500

    crud working with jpql 1

---
+++ src/main/java/edu/wctc/distjava/jgl/bookwebapp/model/AuthorServiceOld.java
@@ -0,0 +1,70 @@
+package edu.wctc.distjava.jgl.bookwebapp.model;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+import javax.persistence.TypedQuery;
+
+@Stateless
+
+// service class that talks to AuthorDao and MySqlDataAccess
+public class AuthorServiceOld implements Serializable {
+
+            Date date = new Date();
+
+    
+    @PersistenceContext(unitName = "book_PU")
+    private EntityManager em;
+
+    public AuthorServiceOld() {
+    }
+
+    public EntityManager getEm() {
+        return em;
+    }
+
+    public void setEm(EntityManager em) {
+        this.em = em;
+    }
+
+    // retrives a list of the author table
+    public final List<Author> getAuthorList() throws Exception {
+
+        List<Author> authorList = new ArrayList<>();
+        String jpql = "select a from Author a";
+        TypedQuery<Author> q = getEm().createQuery(jpql, Author.class);
+        q.setMaxResults(500); // optional
+        authorList = q.getResultList();
+        return authorList;
+    }
+
+    public void createAuthor(String name) {
+        Query query = getEm().createNativeQuery("INSERT INTO Author (author_name, date_added) " + " VALUES(?,?)");
+        query.setParameter(1, name);
+        query.setParameter(2, date);
+        query.executeUpdate();
+    }
+    
+    public void updateAuthorById(String id, String name) {
+        Author author = getEm().find(Author.class, new Integer(id));
+                author.setAuthorName(name);
+                author.setDateAdded(date);
+                getEm().merge(author);
+    }
+
+    public int deleteAuthorById(String id) throws Exception {
+        Integer value = Integer.parseInt(id);
+
+        int recsDeleted = getEm().createQuery("delete from Author a where a.authorId=:id")
+                .setParameter("id", value)
+                .executeUpdate();
+
+        return recsDeleted;
+    }
+
+}
